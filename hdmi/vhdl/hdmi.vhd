63461ebbafd98ab465c7d770d80b4eb699773e2f
provoostkris <provoostkris@hotmail.com>
2021-08-07 16:41:20 +0200

    added two implementation options

--- hdmi/vhdl/hdmi.vhd
+++ hdmi/vhdl/hdmi.vhd
@@ -3,8 +3,12 @@
 library ieee;
 use     ieee.std_logic_1164.all;
 use     ieee.numeric_std.all;
+use     ieee.std_logic_misc.all;
 
 entity hdmi is
+  generic (
+    g_imp             : in    natural range 0 to 1 := 0
+  );
   port (
     FPGA_CLK1_50      : in    std_ulogic; --! FPGA clock 1 input 50 MHz
     FPGA_CLK2_50      : in    std_ulogic; --! FPGA clock 2 input 50 MHz
@@ -84,22 +88,35 @@ signal clk_pll_50  : std_logic;
 signal clk_pll_125 : std_logic;
 signal pll_locked  : std_logic;
 
-signal hs_out       : std_logic;
-signal vs_out       : std_logic;
-signal de_out       : std_logic;
-signal rgb          : std_logic_vector(23 downto 0);
+-- local signals
 signal video_active : std_logic;
+signal i2c_rdy      : std_logic;
+signal i2c_hold     : std_logic;
+signal hdmi_hold    : std_logic;
+signal key_db       : std_logic_vector(1 downto 0);
+signal sw_db        : std_logic_vector(3 downto 0);
 
-signal i2c_rdy  : std_logic;
+-- signal that need to drive the HDcontroller
+type   t_a_std      is array ( integer range <> ) of std_logic;
+type   t_a_slv_24   is array ( integer range <> ) of std_logic_vector(23 downto 0); 
+
+signal clk_out      : t_a_std(0 to 1);
+signal hs_out       : t_a_std(0 to 1);
+signal vs_out       : t_a_std(0 to 1);
+signal de_out       : t_a_std(0 to 1);
+signal rgb          : t_a_slv_24(0 to 1);
+
 
 begin
 
 --! top level assigments
 led(1)                  <= i2c_rdy;
-led(2)                  <= SW(1);
-led(3)                  <= SW(2);
-led(4)                  <= SW(3);
-led(led'high downto 5)  <= ( others => '0');
+led(2)                  <= i2c_hold;
+led(3)                  <= hdmi_hold;
+led(4)                  <= sw_db(0);
+led(5)                  <= sw_db(1);
+led(6)                  <= sw_db(2);
+led(7)                  <= sw_db(3);
 
 --! syncronous resets
 p_rst_pll_25: process (clk_pll_25, pll_locked)
@@ -125,6 +142,7 @@ begin
   end if;
 end process p_rst_pll_50;
 
+-- general purpose pll, generate some clocks
 i_pll : pll
   port map (
     refclk   => FPGA_CLK1_50,
@@ -135,67 +153,144 @@ i_pll : pll
     locked   => pll_locked
   );
 
+--! get rid of the bounce
+p_bounce: process (clk_pll_25, rst_pll_25)
+  variable v_cnt   : unsigned(20 downto 0);
+  variable v_sw_0  : std_logic_vector( 7 downto 0);
+  variable v_sw_1  : std_logic_vector( 7 downto 0);
+  variable v_sw_2  : std_logic_vector( 7 downto 0);
+  variable v_sw_3  : std_logic_vector( 7 downto 0);
+  variable v_key_0 : std_logic_vector( 7 downto 0);
+  variable v_key_1 : std_logic_vector( 7 downto 0);
+begin
+  if rst_pll_25 = '1' then
+    v_cnt   := ( others => '0');
+    v_sw_0  := ( others => '0');
+    v_sw_1  := ( others => '0');
+    v_sw_2  := ( others => '0');
+    v_sw_3  := ( others => '0');
+    v_key_0 := ( others => '0');
+    v_key_1 := ( others => '0');
+    sw_db   <= ( others => '0');
+    key_db  <= ( others => '0');
+  elsif rising_edge(clk_pll_25) then
+    v_cnt    := v_cnt + 1;
+    if or_reduce(std_logic_vector(v_cnt)) = '1' then
+      v_sw_0  := v_sw_0 (v_sw_0'high-1 downto 0)   & SW(0);
+      v_sw_1  := v_sw_1 (v_sw_1'high-1 downto 0)   & SW(1);
+      v_sw_2  := v_sw_2 (v_sw_2'high-1 downto 0)   & SW(2);
+      v_sw_3  := v_sw_3 (v_sw_3'high-1 downto 0)   & SW(3);
+      v_key_0 := v_key_0(v_key_0'high-1 downto 0)  & KEY(0);
+      v_key_1 := v_key_1(v_key_1'high-1 downto 0)  & KEY(1);
+    end if;
+    sw_db(0)  <= or_reduce(v_sw_0 );
+    sw_db(1)  <= or_reduce(v_sw_1 );
+    sw_db(2)  <= or_reduce(v_sw_2 );
+    sw_db(3)  <= or_reduce(v_sw_3 );
+    key_db(0) <= or_reduce(v_key_0);
+    key_db(1) <= or_reduce(v_key_1);
+  end if;
+end process p_bounce;
 
-i_vgaHdmi: vgaHdmi  
-  port map(
-  clock      => clk_pll_25,
-  clock50    => clk_pll_50,
-  reset      => rst_pll_25,
-  
-  switchR    => SW(1),
-  switchG    => SW(2),
-  switchB    => SW(3),
-  
-  hsync      => HDMI_TX_HS,
-  vsync      => HDMI_TX_VS,
-  dataEnable => HDMI_TX_DE,
-  vgaClock   => HDMI_TX_CLK,
-  RGBchannel => HDMI_TX_D
-);
+--!
+--! implementation (0)
+--!
 
-
--- i_timing_generator: entity work.timing_generator(rtl)
-  -- generic map (
-    -- RESOLUTION  => "VGA", 
-    -- GEN_PIX_LOC => false, 
-    -- OBJECT_SIZE => 16
-    -- )
-  -- port map (
-    -- rst           => rst_pll_25, 
-    -- clk           => clk_pll_25, 
-    -- hsync         => hs_out, 
-    -- vsync         => vs_out, 
-    -- video_active  => video_active, 
-    -- pixel_x       => open, 
-    -- pixel_y       => open
-  -- );
-
--- i_pattern_generator: entity work.pattern_generator(rtl)
-  -- port map (
-    -- rst           =>  rst_pll_25,
-    -- clk           =>  clk_pll_25, 
-    -- video_active  =>  video_active, 
-    -- rgb           =>  rgb
-    -- );
+  i_vgaHdmi: vgaHdmi  
+    port map(
+    clock      => clk_pll_25,
+    clock50    => clk_pll_50,
+    reset      => not hdmi_hold,
     
-    -- HDMI_TX_CLK <= clk_pll_25;
+    switchR    => SW(1),
+    switchG    => SW(2),
+    switchB    => SW(3),
+    
+    hsync      => hs_out(0),
+    vsync      => vs_out(0),
+    dataEnable => de_out(0),
+    vgaClock   => clk_out(0),
+    RGBchannel => rgb(0)
+  );
 
---! register outputs
--- p_driver: process (clk_pll_25, rst_pll_25)
--- begin
-  -- if rst_pll_25 = '1' then
-    -- HDMI_TX_D                   <= ( others => '0');
-    -- HDMI_TX_HS                  <= '0';
-    -- HDMI_TX_VS                  <= '0';
-    -- HDMI_TX_DE                  <= '0';
-  -- elsif rising_edge(clk_pll_25) then
-    -- HDMI_TX_D                   <= "10001000" & "01110111" & "00110011";
-    -- HDMI_TX_HS                  <= hs_out;
-    -- HDMI_TX_VS                  <= vs_out;
-    -- HDMI_TX_DE                  <= video_active;
-  -- end if;
--- end process p_driver;
+--!
+--! implementation (1)
+--!
 
+  i_timing_generator: entity work.timing_generator(rtl)
+    generic map (
+      RESOLUTION  => "VGA", 
+      GEN_PIX_LOC => false, 
+      OBJECT_SIZE => 16
+      )
+    port map (
+      rst           => not hdmi_hold,
+      clk           => clk_pll_25, 
+      hsync         => hs_out(1), 
+      vsync         => vs_out(1), 
+      video_active  => video_active, 
+      pixel_x       => open, 
+      pixel_y       => open
+    );
+
+  i_pattern_generator: entity work.pattern_generator(rtl)
+    port map (
+      rst           =>  not hdmi_hold,
+      clk           =>  clk_pll_25, 
+      video_active  =>  video_active, 
+      rgb           =>  rgb(1)
+      );
+      
+      de_out(1)  <= video_active;
+      clk_out(1) <= not clk_pll_25;
+      
+
+--!
+--! select between different implementations
+--! 
+
+  p_driver: process (clk_pll_25, hdmi_hold)
+  begin
+    if hdmi_hold = '0' then
+      HDMI_TX_D                   <= ( others => '0');
+      HDMI_TX_HS                  <= '1';
+      HDMI_TX_VS                  <= '1';
+      HDMI_TX_DE                  <= '0';
+    elsif rising_edge(clk_pll_25) then
+      HDMI_TX_D                   <= rgb(g_imp);
+      HDMI_TX_HS                  <= hs_out(g_imp);
+      HDMI_TX_VS                  <= vs_out(g_imp);
+      HDMI_TX_DE                  <= de_out(g_imp);
+    end if;
+  end process p_driver;
+  
+  -- select the clock when reset is done
+  HDMI_TX_CLK <= clk_out(g_imp) and hdmi_hold;
+
+--!
+--! setup for the registers in the HDMI controller
+--!
+
+--! the i2c has to wait > 200 ms before acessing the bus
+--! we will also wait a moment for the i2c to complete after reset
+p_wait: process (clk_pll_25, rst_pll_25)
+  variable v_cnt : unsigned(24 downto 0);
+  variable v_del : unsigned(25 downto 0);
+begin
+  if rst_pll_25 = '1' then
+    i2c_hold  <= '0';
+    hdmi_hold <= '0';
+    v_cnt     := ( others => '0');
+    v_del     := ( others => '0');
+  elsif rising_edge(clk_pll_25) then
+    v_cnt     := v_cnt + 1;
+    v_del     := v_del + 1;
+    i2c_hold  <= v_cnt(v_cnt'high) or i2c_hold;
+    hdmi_hold <= v_del(v_del'high) or hdmi_hold;
+  end if;
+end process p_wait;
+
+--! the actual controller
 i_i2c_hdmi_config: i2c_hdmi_config
   generic map(
     CLK_Freq    => 50_000_000,
@@ -203,7 +298,7 @@ i_i2c_hdmi_config: i2c_hdmi_config
   )
   port map (
     iCLK        => clk_pll_50,
-    iRST_N      => rst_pll_50_n,
+    iRST_N      => i2c_hold,
     
     I2C_SCLK    => HDMI_I2C_SCL,
     I2C_SDAT    => HDMI_I2C_SDA,
@@ -217,7 +312,7 @@ p_led: process (clk_pll_25, rst_pll_25)
 begin
   if rst_pll_25 = '1' then
     led(0)   <= '0';
-    v_cnt    := ( others => '1');
+    v_cnt    := ( others => '0');
   elsif rising_edge(clk_pll_25) then
     led(0)   <= v_cnt(v_cnt'high);
     v_cnt    := v_cnt + 1;
