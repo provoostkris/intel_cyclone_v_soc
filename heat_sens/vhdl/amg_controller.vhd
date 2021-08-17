15ed1fa6f82ab3d903ddb5c887299d555adeda0b
provoostkris <provoostkris@hotmail.com>
2021-08-17 22:44:58 +0200

    Adding initial design files

---
+++ heat_sens/vhdl/amg_controller.vhd
@@ -0,0 +1,198 @@
+------------------------------------------------------------------------------
+--  design file for amg8833 controller <> terrasic de10 nano cyclone 5 design
+--  rev. 1.0 : 2020 provoost kris
+------------------------------------------------------------------------------
+
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+
+entity amg_controller is
+  port(
+    clk       : in      std_logic;                    --system clock
+    reset_n   : in      std_logic;                    --active low reset
+    ena       : out     std_logic;                    --latch in command
+    addr      : out     std_logic_vector(6 downto 0); --address of target slave
+    rw        : out     std_logic;                    --'0' is write, '1' is read
+    data_wr   : out     std_logic_vector(7 downto 0); --data to write to slave
+    busy      : in      std_logic;                    --indicates transaction in progress
+    data_rd   : in      std_logic_vector(7 downto 0); --data read from slave
+    ack_error : in      std_logic                     --flag if improper acknowledge from slave
+  );
+end amg_controller;
+
+architecture rtl of amg_controller is
+--!
+  constant c_busy_is_done         : std_logic_vector(3 downto 0) := "0000";
+  constant c_busy_is_over         : std_logic_vector(3 downto 0) := "1100";
+  constant c_busy_is_busy         : std_logic_vector(3 downto 0) := "1111";
+
+--! amg8833 definitiois
+  constant c_amg_addr             : std_logic_vector(7 downto 0) := x"68";
+  --! register addresses
+  constant c_amg_register_pctl    : std_logic_vector(7 downto 0) := x"00"; --! power control register
+  constant c_amg_register_rst     : std_logic_vector(7 downto 0) := x"01"; --! reset register
+  constant c_amg_register_fpsc    : std_logic_vector(7 downto 0) := x"02"; --! frame rate register
+  constant c_amg_register_intc    : std_logic_vector(7 downto 0) := x"03"; --! interrupt control register
+  constant c_amg_register_ave     : std_logic_vector(7 downto 0) := x"07"; --! interrupt control register
+  constant c_amg_register_t01l    : std_logic_vector(7 downto 0) := x"80"; --! pixel 1 output value (lower level)
+  constant c_amg_register_t01h    : std_logic_vector(7 downto 0) := x"81"; --! pixel 1 output value (higher level)
+  constant c_amg_register_tthl    : std_logic_vector(7 downto 0) := x"0e"; --! thermistor output value (lower level)
+  constant c_amg_register_tthh    : std_logic_vector(7 downto 0) := x"0f"; --! thermistor output value (higher level)
+  --! power control register values
+  constant c_amg_pctl_normal      : std_logic_vector(7 downto 0) := x"00"; --! normal operating mode
+  constant c_amg_pctl_sleep       : std_logic_vector(7 downto 0) := x"10"; --! sleep mode
+  constant c_amg_pctl_standby10   : std_logic_vector(7 downto 0) := x"21"; --! stand-by (10 sec intermittence)
+  constant c_amg_pctl_standby60   : std_logic_vector(7 downto 0) := x"20"; --! stand-by (60 sec intermittence)
+  --! reset register values
+  constant c_amg_rst_flagreset    : std_logic_vector(7 downto 0) := x"30"; --! flag reset
+  constant c_amg_rst_initialreset : std_logic_vector(7 downto 0) := x"30"; --! flag reset
+  --! frame rate control register values
+  constant c_amg_fpsc_framerate_10: std_logic_vector(7 downto 0) := x"00"; --! 10 fps measurement
+  constant c_amg_fpsc_framerate_1 : std_logic_vector(7 downto 0) := x"01"; --!  1 fps measurement
+
+--! constrol signals
+  type t_state is(idle,
+                  amg_reset,
+                  amg_initialize,
+                  amg_frame_rate,
+                  amg_frame_rate_val,
+                  amg_wake_up,
+                  amg_read_pixels_addr,
+                  amg_read_pixels_low,
+                  amg_read_pixels_high,
+                  amg_process_area,
+                  amg_read_done
+                  );
+
+  signal state         : t_state;                        --state machine
+  signal busy_shift    : std_logic_vector(3 downto 0);   --shift register with busy port
+
+--! RAW pixel values
+  constant c_pixel_area : integer := 64;
+  type   t_a_slv_16     is array ( integer range <> ) of std_logic_vector(15 downto 0);
+  signal heat_values    : t_a_slv_16(0 to c_pixel_area-1);
+  signal cnt_pix        : integer range 0 to c_pixel_area;
+
+begin
+
+
+  -- put the busy signal in a shift register for edge detect
+  process(clk, reset_n)
+  begin
+    if(reset_n = '0') then
+      busy_shift  <= ( others => '0');
+    elsif(clk'event and clk = '1') then
+      busy_shift  <= busy_shift(busy_shift'high-1 downto 0) & busy;
+    end if;
+  end process;
+
+  --state machine control
+  process(clk, reset_n)
+  begin
+    if(reset_n = '0') then
+      state     <= idle;
+      ena       <= '0';
+      addr      <= ( others => '0');
+      rw        <= '0';
+      data_wr   <= ( others => '0');
+      cnt_pix   <= 0;
+    elsif(clk'event and clk = '1') then
+
+        -- set address for chip
+        addr      <= c_amg_addr(addr'range);
+
+        case state is
+          when idle =>
+            -- state control
+            if busy = '0' then
+              state   <=  amg_reset;
+            end if;
+          when amg_reset =>
+            rw        <= '0';
+            data_wr   <= c_amg_register_rst;
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_initialize;
+            end if;
+          when amg_initialize =>
+            rw        <= '0';
+            data_wr   <= c_amg_rst_initialreset;
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_frame_rate;
+            end if;
+          when amg_frame_rate =>
+            rw        <= '0';
+            data_wr   <= c_amg_register_fpsc;
+                        --c_amg_fpsc_framerate_1
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_frame_rate_val;
+            end if;
+          when amg_frame_rate_val =>
+            rw        <= '0';
+            data_wr   <= c_amg_fpsc_framerate_1;
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_wake_up;
+            end if;
+          when amg_wake_up =>
+            rw        <= '0';
+            data_wr   <= c_amg_pctl_normal;
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_read_pixels_addr;
+            end if;
+          when amg_read_pixels_addr =>
+          -- 64 x
+            rw        <= '0';
+            data_wr   <= c_amg_register_t01l;
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_read_pixels_low;
+            end if;
+          when amg_read_pixels_low =>
+            rw        <= '1';
+            data_wr   <= ( others => '0');
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_read_pixels_high;
+            end if;
+            -- store the heat value
+            heat_values(cnt_pix)( 7 downto 0) <= data_rd;
+          when amg_read_pixels_high =>
+            rw        <= '1';
+            data_wr   <= ( others => '0');
+            ena       <= not busy_shift(busy_shift'high) ;
+            -- state control
+            if busy_shift = c_busy_is_over then
+              state   <=  amg_process_area;
+            end if;
+            -- store the heat value
+            heat_values(cnt_pix)(15 downto 8) <= data_rd;
+          when amg_process_area =>
+            if cnt_pix < c_pixel_area then 
+              cnt_pix <=  cnt_pix + 1;
+              state   <=  amg_read_pixels_addr;
+            else
+              state   <=  amg_read_done;
+            end if;
+          when amg_read_done =>
+            -- do some stuff with the pixel area
+            -- and return to idle
+
+          when others =>
+              state   <=  idle;
+        end case;
+      end if;
+  end process;
+
+end rtl;
---
+++ heat_sens/vhdl/heat_sens.vhd
@@ -0,0 +1,178 @@
+------------------------------------------------------------------------------
+--  TOP level design file for AMG8833 controller <> Terrasic DE10 nano Cyclone 5 design
+--  rev. 1.0 : 2020 Provoost Kris
+------------------------------------------------------------------------------
+
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+use     ieee.std_logic_misc.all;
+
+entity heat_sens is
+  generic (
+    g_imp             : in    natural range 0 to 1 := 1
+  );
+  port (
+    FPGA_CLK1_50      : in    std_ulogic; --! FPGA clock 1 input 50 MHz
+    FPGA_CLK2_50      : in    std_ulogic; --! FPGA clock 2 input 50 MHz
+    FPGA_CLK3_50      : in    std_ulogic; --! FPGA clock 3 input 50 MHz
+    -- Buttons & LEDs
+    KEY               : in    std_logic_vector(1 downto 0); --! Push button - debounced
+    SW                : in    std_logic_vector(3 downto 0); --! Slide button
+    Led               : out   std_logic_vector(7 downto 0); --! indicators
+    -- ADV7513
+    AMG_I2C_SCL       : inout std_logic; -- i2c
+    AMG_I2C_SDA       : inout std_logic  -- i2c
+
+  );
+end;
+
+architecture rtl of heat_sens is
+
+
+--! definition of the altera IP
+component pll is
+  port (
+    refclk   : in  std_logic;        -- clk
+    rst      : in  std_logic;        -- reset
+    outclk_0 : out std_logic;        -- clk
+    outclk_1 : out std_logic;        -- clk
+    outclk_2 : out std_logic;        -- clk
+    locked   : out std_logic         -- export
+  );
+end component pll;
+
+signal rst_pll_25     : std_logic;
+signal rst_pll_25_n   : std_logic;
+signal rst_pll_40     : std_logic;
+signal rst_pll_40_n   : std_logic;
+signal rst_pll_50     : std_logic;
+signal rst_pll_50_n   : std_logic;
+
+signal clk_pll_25     : std_logic;
+signal clk_pll_40     : std_logic;
+signal clk_pll_50     : std_logic;
+
+-- local signals
+signal pll_locked     : std_logic;
+signal ena            : std_logic;                    --latch in command
+signal addr           : std_logic_vector(6 downto 0); --address of target slave
+signal rw             : std_logic;                    --'0' is write, '1' is read
+signal data_wr        : std_logic_vector(7 downto 0); --data to write to slave
+signal busy           : std_logic;                    --indicates transaction in progress
+signal data_rd        : std_logic_vector(7 downto 0); --data read from slave
+signal ack_error      : std_logic;                    --flag if improper acknowledge from slave
+
+begin
+
+--! top level assigments
+led(1)                  <= '0';
+led(2)                  <= '0';
+led(3)                  <= '0';
+led(4)                  <= '0';
+led(5)                  <= '0';
+led(6)                  <= '0';
+led(7)                  <= '0';
+
+--! syncronous resets
+p_rst_pll_25: process (clk_pll_25, pll_locked)
+begin
+  if pll_locked = '0' then
+    rst_pll_25   <= '1';
+    rst_pll_25_n <= '0';
+  elsif rising_edge(clk_pll_25) then
+    rst_pll_25   <= '0';
+    rst_pll_25_n <= '1';
+  end if;
+end process p_rst_pll_25;
+
+--! syncronous resets
+p_rst_pll_40: process (clk_pll_40, pll_locked)
+begin
+  if pll_locked = '0' then
+    rst_pll_40   <= '1';
+    rst_pll_40_n <= '0';
+  elsif rising_edge(clk_pll_40) then
+    rst_pll_40   <= '0';
+    rst_pll_40_n <= '1';
+  end if;
+end process p_rst_pll_40;
+
+--! syncronous resets
+p_rst_pll_50: process (clk_pll_50, pll_locked)
+begin
+  if pll_locked = '0' then
+    rst_pll_50   <= '1';
+    rst_pll_50_n <= '0';
+  elsif rising_edge(clk_pll_50) then
+    rst_pll_50   <= '0';
+    rst_pll_50_n <= '1';
+  end if;
+end process p_rst_pll_50;
+
+
+--! general purpose pll, generate some clocks
+i_pll : pll
+  port map (
+    refclk   => FPGA_CLK1_50,
+    rst      => SW(0),
+    outclk_0 => clk_pll_25,       --!  25 MHz
+    outclk_1 => clk_pll_40,       --!  40 MHz
+    outclk_2 => clk_pll_50,       --!  50 MHz
+    locked   => pll_locked
+  );
+
+--!
+--! adding the i2c master
+--!
+i_i2c_master: entity work.i2c_master
+  generic map(
+    input_clk => 25_000_000,              --input clock speed from user logic in hz
+    bus_clk   => 400_000                  --speed the i2c bus (scl) will run at in hz
+    )
+  port map(
+    clk       => clk_pll_25      ,        --system clock
+    reset_n   => rst_pll_25_n    ,        --active low reset
+    ena       => ena             ,        --latch in command
+    addr      => addr            ,        --address of target slave
+    rw        => rw              ,        --'0' is write, '1' is read
+    data_wr   => data_wr         ,        --data to write to slave
+    busy      => busy            ,        --indicates transaction in progress
+    data_rd   => data_rd         ,        --data read from slave
+    ack_error => ack_error       ,        --flag if improper acknowledge from slave
+    sda       => AMG_I2C_SDA     ,        --serial data output of i2c bus
+    scl       => AMG_I2C_SCL              --serial clock output of i2c bus
+    );
+
+--!
+--! adding the i2c controller
+--!
+i_amg_controller: entity work.amg_controller
+  port map(
+    clk       => clk_pll_25      ,        --system clock
+    reset_n   => rst_pll_25_n    ,        --active low reset
+    ena       => ena             ,        --latch in command
+    addr      => addr            ,        --address of target slave
+    rw        => rw              ,        --'0' is write, '1' is read
+    data_wr   => data_wr         ,        --data to write to slave
+    busy      => busy            ,        --indicates transaction in progress
+    data_rd   => data_rd         ,        --data read from slave
+    ack_error => ack_error                --flag if improper acknowledge from slave
+    );
+
+
+
+--! just blink LED to see activity
+p_led: process (clk_pll_25, rst_pll_25)
+  variable v_cnt : unsigned(24 downto 0);
+begin
+  if rst_pll_25 = '1' then
+    led(0)   <= '0';
+    v_cnt    := ( others => '0');
+  elsif rising_edge(clk_pll_25) then
+    led(0)   <= v_cnt(v_cnt'high);
+    v_cnt    := v_cnt + 1;
+  end if;
+end process p_led;
+
+end architecture rtl;
