17c7a63cea6bfb33efada551f89a5d4abb928c66
provoostkris <provoostkris@hotmail.com>
2023-04-06 14:44:50 +0200

    make use of package

--- aes/vhdl/trf_subbytes.vhd
+++ aes/vhdl/trf_subbytes.vhd
@@ -33,7 +33,7 @@ begin
   process(reset_n, clk) is
   begin
       if reset_n='0' then
-        null;
+        in_bytes_i  <= ( others => ( others => '0'));
       elsif rising_edge(clk) then
         in_bytes_i  <= f_slv_to_bytes(subbytes_s);
       end if;
@@ -41,10 +41,10 @@ begin
 
 gen_subbytes: for j in 0 to c_arr-1 generate
   --! use Sbox
-  i_sbox : entity work.sbox
+  i_sbox : entity work.sbox(aes_nor)
     port map(
-      input_byte  => in_bytes_i(j),
-      output_byte => out_bytes_i(j)
+      lookup  => in_bytes_i(j),
+      result  => out_bytes_i(j)
     );
 end generate;
 
@@ -52,7 +52,7 @@ end generate;
   process(reset_n, clk) is
   begin
       if reset_n='0' then
-        null;
+        subbytes_m  <= ( others => '0');
       elsif rising_edge(clk) then
         subbytes_m  <= f_bytes_to_slv(out_bytes_i);
       end if;
